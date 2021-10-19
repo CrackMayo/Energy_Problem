3bad277e10b7e7bfe902c4038de7674d04d23dda
CrackMayo <tamayo998@hotmail.com>
2021-10-18 22:26:53 -0500

    Electric bill variables added

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class energy {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int normalRate = 0;
+        int expensiveRate = 0;
+        int clientsNum = 0;
+        int powerConsumption = 0;
+        int charges = 0;
+        normalRate = sc.nextInt();
+        expensiveRate = sc.nextInt();
+        clientsNum = sc.nextInt();
+
+    }
+}
